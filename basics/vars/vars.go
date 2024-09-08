bdf61e24da81b24f77fb0afb5d5b1fe29efa8b89
Abdelaziz Tina <tinaabdelaziz8@gmail.com>
2024-09-08 14:19:25 +0200

    var assignment

---
+++ basics/vars/vars.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = "Hi!"
+	fmt.Println(a)
+
+	var b int = 6
+	fmt.Println(b)
+	
+	b = 2
+	fmt.Println(b)
+	
+	var e int
+	fmt.Println(e)
+
+	f := 2
+	fmt.Println(f)
+}
