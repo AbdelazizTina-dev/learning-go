24c157b84e17117b2425c845fae01e68a2f7b01c
Abdelaziz Tina <tinaabdelaziz8@gmail.com>
2024-09-25 16:26:32 +0200

    add an error and handle it with logging

--- docs-tutorials/hello/hello.go
+++ docs-tutorials/hello/hello.go
@@ -3,10 +3,18 @@ package main
 import (
 	"fmt"
 	"tutorial/first"
+	"log"
 )
 
 func main() {
+	log.SetFlags(0)
+	log.SetPrefix("greetings: ")
 	// Get a greeting message and print it.
-	message := greetings.Hello("Gladys")
+	message, error := greetings.Hello("")
+
+	if error != nil {
+		log.Fatal(error)
+	}
+
 	fmt.Println(message)
 }
